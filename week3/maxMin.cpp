f4b2f59b6f49ef1c1f416f69928be7563f74c1a6
Alexander Traut <trauta@flip3.engr.oregonstate.edu>
2015-01-25 13:06:26 -0800

    commit try

---
+++ week3/maxMin.cpp
@@ -0,0 +1,36 @@
+/*******************************************
+ * * Name: Alex Traut
+ * * Date: 1/25/15
+ * * Description: Program to sort high and low number from list
+**********************************************/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double totalNumbers, userNumber, highNum, lowNum;
+
+    cout << "How many numbers would you like to enter? \n";
+    cin >> totalNumbers;
+    cout << "Please provide a number. \n";
+
+    // Start looping for user inputted numbers
+    for (int i = 0; i < totalNumbers; i++) {
+        cin >> userNumber;
+        cout << "Please provide another number." << endl;
+        if (i == 0) {
+	    highNum = userNumber;
+	    lowNum = userNumber;
+        }
+        else if (userNumber > highNum) {
+            highNum = userNumber;
+        }
+        else if (userNumber < lowNum) {
+            lowNum = userNumber;
+        }
+    }
+    cout << "Lowest Number: " << lowNum << endl;
+    cout << "Highest Number: " << highNum << endl;
+
+}
